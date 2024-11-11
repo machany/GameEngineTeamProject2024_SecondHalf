1ea643785277bcce0bfe7313261d33a4c9e4244f
jiho08 <57872222k@gmail.com>
2024-11-12 00:52:49 +0900

    style : LineGroup script component

--- Assets/00.Work/KJH/01.Scripts/Line/LineGroup.cs
+++ Assets/00.Work/KJH/01.Scripts/Line/LineGroup.cs
@@ -1,58 +1,60 @@
- using System;
-using System.Collections;
+using System;
 using System.Collections.Generic;
-using System.Diagnostics;
 using UnityEngine;
-using UnityEngine.Serialization;
 
-
- //선로의 색깔
+// 선로 색
 public enum LineGroupType
 {
     Red,
     Blue,
     Green
 }
- 
- //선로가 들오는 것인지 나가는 것 인지 구분하는거
+
+// 선로 타입 구분
 public enum LineType
 {
     Input,
     Output
 }
+
 public class LineGroup : MonoBehaviour
 {
-
-    
     public List<Transform> inputRedPos = new();
-    public List<Transform> inputBluePos = new(); 
+    public List<Transform> inputBluePos = new();
     public List<Transform> inputGreenPos = new();
-    public List<Transform> outputRedPos = new(); 
+
+    public List<Transform> outputRedPos = new();
     public List<Transform> outputBluePos = new();
     public List<Transform> outputGreenPos = new();
-    
-    public List<GameObject> inputRedObjects = new();
-    public List<GameObject> inputBlueObjects = new(); 
-    public List<GameObject> inputGreenObjects = new();
-    public List<GameObject> outputRedObjects = new();
-    public List<GameObject> outputBlueObjects= new();
-    public List<GameObject> outputGreenObjects = new();
-    
-    public GameObject ir, ib, ig, or, ob, og;
 
-    //선로의 색깔을 그룹에 따라 지정해주는 메쏘드
+    public List<GameObject> inputRedObjects = new();
+    public List<GameObject> inputBlueObjects = new();
+    public List<GameObject> inputGreenObjects = new();
+
+    public List<GameObject> outputRedObjects = new();
+    public List<GameObject> outputBlueObjects = new();
+    public List<GameObject> outputGreenObjects = new();
+
+    public GameObject inputRed, inputBlue, inputGreen;
+    public GameObject outputRed, outputBlue, outputGreen;
+    
+    /// <summary> 선로의 색을 그룹에 따라 지정해주는 메서드 </summary>
+    /// <param name="lineType"> 선로 타입 </param>
+    /// <param name="lineGroupType"> 선로 색 </param>
     public GameObject GetLineGroupType(LineType lineType, LineGroupType lineGroupType) => lineGroupType switch
     {
-        LineGroupType.Red when lineType == LineType.Input => ir,
-        LineGroupType.Red => or,
-        LineGroupType.Blue when lineType == LineType.Input => ib,
-        LineGroupType.Blue => ob,
-        LineGroupType.Green when lineType == LineType.Input => ig,
-        LineGroupType.Green => og,
+        LineGroupType.Red when lineType == LineType.Input => inputRed,
+        LineGroupType.Red => outputRed,
+        LineGroupType.Blue when lineType == LineType.Input => inputBlue,
+        LineGroupType.Blue => outputBlue,
+        LineGroupType.Green when lineType == LineType.Input => inputGreen,
+        LineGroupType.Green => outputGreen,
         _ => throw new Exception("wow, you seem like 57")
     };
 
-    //선로를 따라 이동할 운송수단을 위한 운송수단이 이동해야할 위치값을 지정해주는 메쏘드
+    /// <summary> 운송수단이 이동해야 할 위치 값을 지정해주는 메서드 </summary>
+    /// <param name="lineType"> 선로 타입 </param>
+    /// <param name="lineGroupType"> 선로 색 </param>
     public List<Transform> GetLinePosType(LineType lineType, LineGroupType lineGroupType) => lineGroupType switch
     {
         LineGroupType.Red when lineType == LineType.Input => inputRedPos,
@@ -63,8 +65,10 @@ public class LineGroup : MonoBehaviour
         LineGroupType.Green => outputGreenPos,
         _ => throw new Exception("wow, you seem like 5.7")
     };
-
-    //현재 연결된 선로의 그룹을 지정해주는 메쏘드
+    
+    /// <summary> 현재 연결된 선로의 그룹을 지정해주는 메서드 </summary>
+    /// <param name="lineType"> 선로 타입 </param>
+    /// <param name="lineGroupType"> 선로 색 </param>
     public List<GameObject> GetLineType(LineType lineType, LineGroupType lineGroupType) => lineGroupType switch
     {
         LineGroupType.Red when lineType == LineType.Input => inputRedObjects,
@@ -75,6 +79,7 @@ public class LineGroup : MonoBehaviour
         LineGroupType.Green => outputGreenObjects,
         _ => throw new Exception("wow, you seem like 570")
     };
+
     public List<GameObject> GetAllLines()
     {
         List<GameObject> allLines = new List<GameObject>();
@@ -88,7 +93,3 @@ public class LineGroup : MonoBehaviour
         return allLines;
     }
 }
-
-
-
-
