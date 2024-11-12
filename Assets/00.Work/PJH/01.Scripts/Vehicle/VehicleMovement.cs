f11bda8678e59146571e4f40a9c3116e95118b69
jiho08 <57872222k@gmail.com>
2024-11-12 10:32:35 +0900

    remove

--- Assets/00.Work/PJH/01.Scripts/Vehicle/VehicleMovement.cs
+++ Assets/00.Work/PJH/01.Scripts/Vehicle/VehicleMovement.cs
@@ -5,4 +5,6 @@ using UnityEngine;
 public class VehicleMovement : MonoBehaviour
 {
     private LineGroup _lineGroup;
+    private LineGroupType _currentLineGroupType;
+    private LineType _currentLineType;
 }
