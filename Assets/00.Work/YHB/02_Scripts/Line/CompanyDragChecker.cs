3cabf02b8041fbae940439461efad31bb4e79119
machany <machanicaly@gmail.com>
2024-11-26 20:02:23 +0900

    refactor : LineInput, style

--- Assets/00.Work/YHB/02_Scripts/Line/CompanyDragChecker.cs
+++ Assets/00.Work/YHB/02_Scripts/Line/CompanyDragChecker.cs
@@ -11,13 +11,18 @@ public class CompanyDragChecker : MonoBehaviour
     [SerializeField] private float equalsBuildingCoolTime = 1f;
     [SerializeField] private LayerMask buildingLayer;
 
-    private bool equalsBuildingCool;
-    private GameObject building;
+    private bool _equalsBuildingCool;
+    private bool _destroyMod;
+    private GameObject _building;
 
-    public void SetMouseClick()
+    public void SetMouseClick(Vector3 position)
     {
+        LineController.Instance.ResetCurTrm();
         StopAllCoroutines();
-        equalsBuildingCool = false;
+        _equalsBuildingCool = false;
+
+        Collider2D collision = Physics2D.OverlapCircle(position, radius, buildingLayer);
+        _destroyMod = collision is not null;
     }
 
     public void CheckBuilding(Vector3 position)
@@ -25,27 +30,20 @@ public class CompanyDragChecker : MonoBehaviour
         Collider2D collision = Physics2D.OverlapCircle(position, radius, buildingLayer);
         if (collision is not null)
         {
-            if (EqualityComparer<GameObject>.Default.Equals(building, collision.gameObject))
-            {
-                if (!equalsBuildingCool)
-                {
-                    StartCoroutine(BuildingCoolTime(collision.gameObject));
-                    OnBuilding?.Invoke(collision.gameObject);
-                }
-            }
-            else
-            {
-                StartCoroutine(BuildingCoolTime(collision.gameObject));
-                OnBuilding?.Invoke(collision.gameObject);
-            }
+            if (EqualityComparer<GameObject>.Default.Equals(_building, collision.gameObject)
+                && _equalsBuildingCool && !_destroyMod)
+                return;
+
+            StartCoroutine(BuildingCoolTime(collision.gameObject));
+            OnBuilding?.Invoke(collision.gameObject);
         }
     }
 
     private IEnumerator BuildingCoolTime(GameObject collision)
     {
-        equalsBuildingCool = true;
-        building = collision;
+        _equalsBuildingCool = true;
+        _building = collision;
         yield return new WaitForSeconds(equalsBuildingCoolTime);
-        equalsBuildingCool = false;
+        _equalsBuildingCool = false;
     }
 }
