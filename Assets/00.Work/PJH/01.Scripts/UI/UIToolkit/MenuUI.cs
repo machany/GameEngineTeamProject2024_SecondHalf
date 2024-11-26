d0d3a06f22559a96f5316b132475c04f0fdb8fdf
jiho08 <57872222k@gmail.com>
2024-11-26 10:11:51 +0900

    feat : TitleUI, GameManager, FixedScreen, FadeManager

---
+++ Assets/00.Work/PJH/01.Scripts/UI/UIToolkit/MenuUI.cs
@@ -0,0 +1,66 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class MenuUI : UIToolkit
+{
+    private const string _startStr = "Button_Start";
+    private const string _guideStr = "Button_Guide";
+    private const string _settingStr = "Button_Setting";
+    private const string _exitStr = "Button_Exit";
+
+    private Button _startButton;
+    private Button _guideButton;
+    private Button _settingButton;
+    private Button _exitButton;
+
+    private void OnEnable()
+    {
+        GetUIElements();
+
+        _startButton.clicked += ClickStartButton;
+        _guideButton.clicked += ClickGuideButton;
+        _settingButton.clicked += ClickSettingButton;
+        _exitButton.clicked += ClickExitButton;
+    }
+
+    private void OnDisable()
+    {
+        _startButton.clicked -= ClickStartButton;
+        _guideButton.clicked -= ClickGuideButton;
+        _settingButton.clicked -= ClickSettingButton;
+        _exitButton.clicked -= ClickExitButton;
+    }
+
+    protected override void GetUIElements()
+    {
+        base.GetUIElements();
+
+        _startButton = root.Q<Button>(_startStr);
+        _guideButton = root.Q<Button>(_guideStr);
+        _settingButton = root.Q<Button>(_settingStr);
+        _exitButton = root.Q<Button>(_exitStr);
+    }
+
+    private void ClickStartButton()
+    {
+        // 이동
+    }
+
+    private void ClickGuideButton()
+    {
+        // 튜토리얼 씬 전환 FadeManager.FadeIn
+    }
+
+    private void ClickSettingButton()
+    {
+        // 이동
+    }
+
+    private void ClickExitButton()
+    {
+        FadeManager.FadeIn(Application.Quit);
+    }
+}
