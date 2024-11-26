0622099028f5e50bce8114575ff76d2cd0849bbf
김진호 <russiakim2008@gmail.com>
2024-11-26 18:16:57 +0900

    feat : soundSo

--- Assets/00.Work/YHB/02_Scripts/06_Sound/SoundSO.cs
+++ Assets/00.Work/YHB/02_Scripts/06_Sound/SoundSO.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -7,4 +8,9 @@ public class SoundSO : ScriptableObject
 {
     public string key;
     public AudioClip clip;
+
+    private void OnValidate()
+    {
+        key = this.name;
+    }
 }
