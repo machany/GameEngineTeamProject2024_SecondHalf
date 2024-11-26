931d3684b61f651ce54bdb45101dd24db7ab041f
jiho08 <57872222k@gmail.com>
2024-11-26 12:36:55 +0900

    refactor : bridge

--- Assets/00.Work/PJH/01.Scripts/UI/UIToolkit/ResourceUI.cs
+++ Assets/00.Work/PJH/01.Scripts/UI/UIToolkit/ResourceUI.cs
@@ -1,14 +1,10 @@
-using System;
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
 using UnityEngine.UIElements;
 
 public class ResourceUI : UIToolkit
 {
     private const string _resourceStr = "VisualElement_Resource";
     private const string _toggleStr = "Button_Toggle";
-    
+
     private readonly string[] _countStr =
     {
         "Label_Red", "Label_Yellow", "Label_Green", "Label_Blue", "Label_Purple", "Label_Bridge"
@@ -18,9 +14,9 @@ public class ResourceUI : UIToolkit
     private const string _toggleButtonStyle = "button-toggle-2";
 
     private VisualElement _resourceVisualElement;
-    
+
     private Button _toggleButton;
-    
+
     private Label[] _countLabels = new Label[6];
 
     private bool _isToggle;
@@ -47,13 +43,13 @@ public class ResourceUI : UIToolkit
         base.GetUIElements();
 
         _resourceVisualElement = root.Q<VisualElement>(_resourceStr);
-        
+
         _toggleButton = root.Q<Button>(_toggleStr);
 
         for (int i = 0; i < 6; ++i)
             _countLabels[i] = root.Q<Label>(_countStr[i]);
     }
-    
+
     private void SetReSourceLabel()
     {
         _countLabels[0].text = DistributionCenter.Storage[ResourceType.Red].ToString();
@@ -61,18 +57,13 @@ public class ResourceUI : UIToolkit
         _countLabels[2].text = DistributionCenter.Storage[ResourceType.Green].ToString();
         _countLabels[3].text = DistributionCenter.Storage[ResourceType.Blue].ToString();
         _countLabels[4].text = DistributionCenter.Storage[ResourceType.Purple].ToString();
-        _countLabels[5].text = BridgeManager.Instance.AvailableBridge.ToString();
+        _countLabels[5].text = BridgeManager.Instance.AvailableBridgeCount().ToString();
     }
-    
+
     private void ClickToggleButton()
     {
         _isToggle = !_isToggle;
-        
-        Toggle();
-    }
-    
-    private void Toggle()
-    {
+
         _resourceVisualElement.ToggleInClassList(_toggleStyle);
         _toggleButton.ToggleInClassList(_toggleButtonStyle);
     }
