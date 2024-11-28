a6f34d50b7a3a627c0282450481b62e5d6586e80
jiho08 <57872222k@gmail.com>
2024-11-29 00:37:04 +0900

    feat : vehicle UI vehicle count

--- Assets/00.Work/PJH/01.Scripts/UI/UIToolkit/VehicleUI.cs
+++ Assets/00.Work/PJH/01.Scripts/UI/UIToolkit/VehicleUI.cs
@@ -9,19 +9,27 @@ public class VehicleUI : UIToolkit, IInputable
     public static Action<bool> OnCarSelected;
     public static Action<bool> OnTruckSelected;
     public static Action<bool> OnTrailerSelected;
-    
+
     private const string _carStr = "Button_Car";
     private const string _truckStr = "Button_Truck";
     private const string _trailerStr = "Button_Trailer";
-    
+
+    private const string _carCountStr = "Label_CarCount";
+    private const string _truckCountStr = "Label_TruckCount";
+    private const string _trailerCountStr = "Label_TrailerCount";
+
     private Button _carButton;
     private Button _truckButton;
     private Button _trailerButton;
-    
+
+    private Label _carCountLabel;
+    private Label _truckCountLabel;
+    private Label _trailerCountLabel;
+
     private bool _isCarSelected;
     private bool _isTruckSelected;
     private bool _isTrailerSelected;
-    
+
     private void OnEnable()
     {
         GetUIElements();
@@ -29,32 +37,41 @@ public class VehicleUI : UIToolkit, IInputable
         _carButton.clicked += OnCarEvent;
         _truckButton.clicked += OnTruckEvent;
         _trailerButton.clicked += OnTrailerEvent;
-        
+
         InputCompo.OnCarEvent += OnCarEvent;
         InputCompo.OnTruckEvent += OnTruckEvent;
         InputCompo.OnTrailerEvent += OnTrailerEvent;
     }
-    
+
     private void OnDisable()
     {
         _carButton.clicked -= OnCarEvent;
         _truckButton.clicked -= OnTruckEvent;
         _trailerButton.clicked -= OnTrailerEvent;
-        
+
         InputCompo.OnCarEvent -= OnCarEvent;
         InputCompo.OnTruckEvent -= OnTruckEvent;
         InputCompo.OnTrailerEvent -= OnTrailerEvent;
     }
 
+    private void Update()
+    {
+        UpdateVehicleCount();
+    }
+
     protected override void GetUIElements()
     {
         base.GetUIElements();
-        
+
         _carButton = root.Q<Button>(_carStr);
         _truckButton = root.Q<Button>(_truckStr);
         _trailerButton = root.Q<Button>(_trailerStr);
+        
+        _carCountLabel = root.Q<Label>(_carCountStr);
+        _truckCountLabel = root.Q<Label>(_truckCountStr);
+        _trailerCountLabel = root.Q<Label>(_trailerCountStr);
     }
-    
+
     private void OnCarEvent()
     {
         _isCarSelected = !_isCarSelected;
@@ -62,7 +79,7 @@ public class VehicleUI : UIToolkit, IInputable
         _isTrailerSelected = false;
         OnCarSelected?.Invoke(_isCarSelected);
     }
-    
+
     private void OnTruckEvent()
     {
         _isTruckSelected = !_isTruckSelected;
@@ -70,12 +87,19 @@ public class VehicleUI : UIToolkit, IInputable
         _isTrailerSelected = false;
         OnTruckSelected?.Invoke(_isTruckSelected);
     }
-    
+
     private void OnTrailerEvent()
     {
         _isTrailerSelected = !_isTrailerSelected;
         _isCarSelected = false;
         _isTruckSelected = false;
         OnTrailerSelected?.Invoke(_isTrailerSelected);
+    }
+    
+    private void UpdateVehicleCount()
+    {
+        _carCountLabel.text = ResourceManager.Instance.GetVehicleValue(VehicleType.car).ToString();
+        _truckCountLabel.text = ResourceManager.Instance.GetVehicleValue(VehicleType.truck).ToString();
+        _trailerCountLabel.text = ResourceManager.Instance.GetVehicleValue(VehicleType.trailer).ToString();
     }
 }
