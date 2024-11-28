378fd49c8766627b7c0e800cd6ba07564ab137ab
김진호 <russiakim2008@gmail.com>
2024-11-28 18:31:13 +0900

    feat : addPoolEffect

--- Assets/00.Work/KJH/08.Pa/CompanyEffect.cs
+++ Assets/00.Work/KJH/08.Pa/CompanyEffect.cs
@@ -3,10 +3,13 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Drawing;
+using UnityEditorInternal.Profiling.Memory.Experimental;
 using UnityEngine;
 
 public class CompanyEffect : MonoBehaviour
 {
+    [SerializeField] PoolItemSO poolItemSO;
+    
     [SerializeField] private Material _material;
 
     [Header("set")]
@@ -15,6 +18,13 @@ public class CompanyEffect : MonoBehaviour
 
     private int circleSizeID;
     private int lineThickID;
+
+    
+    
+
+    private float effectTime = 1f;
+    private float currentTime = 0;
+    private int companyEffectKey;
 
     [Header("value")]
     [Range(0f, 1f)]
@@ -25,8 +35,15 @@ public class CompanyEffect : MonoBehaviour
     private static bool _isReset;
     private static float _lifeTime;
 
+    private void Awake()
+    {
+        companyEffectKey = poolItemSO.key;
+    }
+
     public void OnEnable()
     {
+
+        StartCoroutine(Pool());
         if (!_isReset)
         {
             _lifeTime = transform.GetComponent<ParticleSystem>().startLifetime;
@@ -42,4 +59,24 @@ public class CompanyEffect : MonoBehaviour
         DOTween.To(() => startCircleSize, size => { _material.SetFloat(circleSizeID, size); }, endCircleSize, _lifeTime);
         DOTween.To(() => startLineThick, size => { _material.SetFloat(lineThickID, size); }, endLineThick, _lifeTime);
     }
+
+    private IEnumerator Pool()
+    {
+        while (effectTime >= currentTime) 
+        {
+            currentTime += Time.deltaTime;
+            yield return new WaitForSeconds(Time.deltaTime);
+        }
+
+        ObjPool();
+
+        currentTime = 0;
+    }
+
+    private void ObjPool()
+    {
+        PoolManager.Instance.Push(companyEffectKey,  gameObject);
+        Debug.Log("pool에 들어감!");
+    }
+    
 }
